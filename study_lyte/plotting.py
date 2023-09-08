5273938aa933452796339779739f6c46792edb9f
micah johnson <micah.johnson150@gmail.com>
2023-09-08 13:37:47 -0600

    Missed tiny ref to styles that were migrated

--- study_lyte/plotting.py
+++ study_lyte/plotting.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-
+from study_lyte.styles import EventStyle
 def plot_events(ax, profile_events, plot_type='normal', event_alpha=0.6):
     """
     Plots the hline or vline for each event on a plot
